e5de36e72880bbaac819bb5368e604cfdf4001c5
Moritz <moritz.laass@gmail.com>
2020-04-18 21:28:36 +0200

    Progress
    * farm looks even better and house is to scale
    * automatic collision meshes from blender export
    * added poo and grass objects with physics and area colliders on different layers
    * added hud labels for cow poo and gras
    *

---
+++ farm_life_godot/objects/gras.gd
@@ -0,0 +1,28 @@
+extends RigidBody
+
+# stuff
+var label 
+var labelAnchor 
+var cam
+
+var timeAlive = 0;
+
+func _process(_delta):
+	timeAlive += _delta
+	## adjust label position
+	var pos = labelAnchor.get_global_transform().origin;
+	var screenPos = cam.unproject_position(pos);
+	label.set_position(screenPos);
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	cam = get_tree().get_root().get_camera()
+	label = get_node("Label")
+	labelAnchor = get_node("LabelAnchor")
+	pass
+
+
+func _on_Area_body_entered(body):
+	print("gras entered")
+	print(body.name)
+	pass # Replace with function body.
