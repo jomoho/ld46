b41ce648926586d61f1a7d565d111aeffbce1cd2
Moritz <moritz.laass@gmail.com>
2020-04-20 11:59:35 +0200

    fixed gui alignment

--- farm_life_godot/scenes/start/start_menu.gd
+++ farm_life_godot/scenes/start/start_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Spatial
 
 export var game_over = false
 
@@ -8,9 +8,10 @@ func _ready():
 	pass 
 
 func load_stats():
-	$stats.set_text( "Stats: %s" % get_node("/root/globals").get_stats())
+	$Control.get_node("stats").set_text( "Stats: %s" % get_node("/root/globals").get_stats())
 	pass
 
 func _on_StartButton_pressed():
+	print("START")
 	get_tree().change_scene("res://scenes/farm/farm_level.tscn")
 	pass 
